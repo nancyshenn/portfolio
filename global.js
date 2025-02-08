cf3d3a71296d72b95d28489e74fcf51c1bcd6454
Nancy Shen <nshen@ucsd.edu>
2025-02-07 22:17:50 -0800

    added components of lab05

--- global.js
+++ global.js
@@ -105,7 +105,10 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
       article.innerHTML = `
           <${headingLevel}>${project.title}</${headingLevel}>
           <img src="${project.image}" alt="${project.title}">
-          <p>${project.description}</p>
+          <div class="project-details">
+              <p>${project.description}</p>
+              <p class="project-year"><em>Year: ${project.year}</em></p>
+          </div>
       `;
       containerElement.appendChild(article);
   });
